bdac0e0d75574670ba01dc121e27766bb41437c5
Shafwannn24 <dheanshafwan24@gmail.com>
2024-11-13 14:18:44 +0700

    finished project

---
+++ app/src/main/java/com/example/evoltruster/login.java
@@ -0,0 +1,142 @@
+package com.example.evoltruster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class login extends AppCompatActivity implements View.OnClickListener {
+
+    // Deklarasi views
+    TextView tvTime, tvDuration;
+    SeekBar seekBarTime, seekBarVolume;
+    ImageView btnPlay;
+
+    MediaPlayer musicPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Sembunyikan ActionBar
+        getSupportActionBar().hide();
+
+        tvTime = findViewById(R.id.tvTime);
+        tvDuration = findViewById(R.id.tvDuration);
+        seekBarTime = findViewById(R.id.seekBarTime);
+        seekBarVolume = findViewById(R.id.seekBarVolume);
+        btnPlay = findViewById(R.id.btnPlay);
+
+        musicPlayer = MediaPlayer.create(this, R.raw.free_sound);
+        musicPlayer.setLooping(true);
+        musicPlayer.seekTo(0);
+        musicPlayer.setVolume(0.5f, 0.5f);
+
+        String duration = millisecondsTostring(musicPlayer.getDuration());
+        tvDuration.setText(duration);
+
+        btnPlay.setOnClickListener(this);
+
+        seekBarVolume.setProgress(50);
+        seekBarVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean isfromUser) {
+                float volume = progress / 100f;
+                musicPlayer.setVolume(volume, volume);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBarTime.setMax(musicPlayer.getDuration());
+        seekBarTime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean isfromUser) {
+                if (isfromUser) {
+                    musicPlayer.seekTo(progress);
+                    seekBar.setProgress(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (musicPlayer != null) {
+                    if (musicPlayer.isPlaying()) {
+                        try {
+                            final double current = musicPlayer.getCurrentPosition();
+                            final String elapsedTime = millisecondsTostring((int) current);
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    tvTime.setText(elapsedTime);
+                                    seekBarTime.setProgress((int) current);
+                                }
+                            });
+
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+    } // akhir method onCreate
+
+
+    public String millisecondsTostring(int time) {
+        String elapsedTime = "";
+        int minutes = time / 1000 / 60;
+        int seconds = time / 1000 % 60;
+        elapsedTime = minutes + ":";
+        if (seconds < 10) {
+            elapsedTime += "0";
+        }
+        elapsedTime += seconds;
+
+        return elapsedTime;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnPlay) {
+            if (musicPlayer.isPlaying()) {
+                // Sedang memutar
+                musicPlayer.pause();
+                btnPlay.setBackgroundResource(R.drawable.buttonplay);
+            } else {
+                // Sedang berhenti
+                musicPlayer.start();
+                btnPlay.setBackgroundResource(R.drawable.buttonpause);
+            }
+        }
+    }
+}
